4b00c6421fba57d443c1420d9fa34e1bb5137c71
JI1IN <1918jas@gmail.com>
2024-08-07 12:15:55 +0200

    Update Tkinter_FileHandler.py

--- Notes/Tkinter_FileHandler.py
+++ Notes/Tkinter_FileHandler.py
@@ -18,10 +18,9 @@ def read_file(text_box):
         messagebox.showerror("Error", f'An error has occurred: {e}')
 
 
-# Function to save the file after user confirmation
 def save_file_after_confirmation(text_box):
     try:
-        content = text_box.get(1.0, tk.END).strip()  # Get the user-entered text and strip leading/trailing whitespace
+        content = text_box.get(1.0, tk.END).strip() 
         if content:
             filename = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text files", "*.txt"),
                                                                                         ("All files", "*.*")])
@@ -37,20 +36,17 @@ def save_file_after_confirmation(text_box):
 
 def write_file(text_box, root):
     text_to_append = "Enter the text you want to append to the file: "
-    # Clear the text box and insert the prompt
+   
     text_box.delete(1.0, tk.END)
     text_box.insert('end', text_to_append)
 
-    # Remove the prompt text and get user input when the button is clicked
     def on_confirm():
-        # Remove the prompt text
         text_box.delete(1.0, "1.{}".format(len(text_to_append)))
         save_file_after_confirmation(text_box)
-        confirm_button.pack_forget()  # Remove the confirm button after saving
+        confirm_button.pack_forget() 
 
-    # Create a confirm button for the user to confirm their input
     confirm_button = ttk.Button(root, text="Confirm and Save", command=on_confirm)
-    confirm_button.pack(pady=10)
+    confirm_button.grid(row=4, column=3, padx=10, pady=10, sticky=tk.W)
 
 
 def delete_file():
